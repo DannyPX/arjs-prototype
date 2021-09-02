6037a4566e9f77053020295f1217f89e098cbff1
Vũ,Danny D <d.vu@student.fontys.nl>
2021-09-02 12:48:20 +0200

    deploy

---
+++ precache-manifest.13bd1771156fc7c2f61299f47fb4e729.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83efe274c21e6b168147",
+    "url": "/arjs-prototype/css/app.6dd0ba7c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/arjs-prototype/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e45dac483c080d1fc83a9c49a2c612e9",
+    "url": "/arjs-prototype/index.html"
+  },
+  {
+    "revision": "82ec0ee409a0ed32ef7c",
+    "url": "/arjs-prototype/js/about.efe0d293.js"
+  },
+  {
+    "revision": "83efe274c21e6b168147",
+    "url": "/arjs-prototype/js/app.9cd381f5.js"
+  },
+  {
+    "revision": "53d4e768a98fa3ef7537",
+    "url": "/arjs-prototype/js/chunk-vendors.1eabd6f8.js"
+  },
+  {
+    "revision": "a6145650711c9e38ca09528de067b7c3",
+    "url": "/arjs-prototype/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/arjs-prototype/robots.txt"
+  }
+]);
